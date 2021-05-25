b022c9a75994156830e7b9bcaa87894897dbbffc
graciedevine <gracie.devine@gmail.com>
2021-05-25 14:01:35 -0400

    RPS is running, but outcomes sometimes unpredictable.

--- rps-game/scripts/main.js
+++ rps-game/scripts/main.js
@@ -1,15 +1,25 @@
 function computerPlay() {
     let moves = ['rock', 'paper', 'scissors'];
-    let computerSelection = moves[Math.floor(Math.random() * moves.length)];
-    return computerSelection
-}
-
-function userPlay() {
-    
+    let computerChoice = moves[Math.floor(Math.random() * moves.length)];
+    return computerChoice
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
-    computerSelection = computerSelection.toLowerCase()
-}
+    let playerMove = prompt('Choose your weapon', 'rock, paper, scissors')
+    let computerMove = computerPlay();
 
+    if (playerMove === 'rock' && computerMove === 'paper') {
+        console.log('Sorry, paper covers rock.')
+    } else if (playerMove === 'paper' && computerMove === 'rock') {
+        console.log('Yay! Paper covers rock!')
+    } else if (playerMove === 'scissors' && computerMove === 'rock') {
+        console.log('Sorry, rock beats scissors.')
+    } else if (playerMove === 'rock' && computerMove === 'scissors') {
+        console.log('Yay! Rock beats scissors!')
+    } else if (playerMove === 'scissors' && computerMove === 'paper') {
+        console.log('Yay! Scissors cut paper!')
+    } else if (playerMove === 'paper' && computerMove === 'scissors')
+        console.log('Sorry, scissors cut paper.')
+
+    return playerSelection, computerSelection
+}
