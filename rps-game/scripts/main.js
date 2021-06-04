b727af5f289296222510cc5fb69aba6abf8b6ad1
graciedevine <gracie.devine@gmail.com>
2021-06-04 11:35:02 -0400

    Plays 5 rounds then terminates. Added alert for invalid input.

--- rps-game/scripts/main.js
+++ rps-game/scripts/main.js
@@ -1,46 +1,41 @@
+const moves = ['rock', 'paper', 'scissors'];
+
 function computerPlay() {
-    let moves = ['rock', 'paper', 'scissors'];
-    let computerMove = moves[Math.floor(Math.random() * moves.length)];
-    return computerMove
+	const computerMove = moves[Math.floor(Math.random() * moves.length)];
+	return computerMove;
 }
 
-function playRound(playerSelection, computerSelection) {
-    let playerChoice = prompt('Choose your weapon', 'rock, paper, scissors')
-    let computerChoice = computerPlay();
+function playRound() {
+	const playerChoice = prompt('Choose your weapon', 'rock, paper, scissors').toLowerCase()
+	if (!moves.includes(playerChoice)) {
+		alert('Invalid move.');
+		return;
+	}
+	const computerChoice = computerPlay();
 
-    if (playerChoice === 'rock' && computerChoice === 'paper') {
-        console.log('Sorry, paper covers rock.')
-    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
-        console.log('Yay! Paper covers rock!')
-    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
-        console.log('Sorry, rock beats scissors.')
-    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
-        console.log('Yay! Rock beats scissors!')
-    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
-        console.log('Yay! Scissors cut paper!')
-    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
-        console.log('Sorry, scissors cut paper.')
-    } else if (playerChoice === computerChoice) {
-        console.log("It's a tie!")
-
-        return playerSelection, computerSelection
-    }
+	if (playerChoice === 'rock' && computerChoice === 'paper') {
+		console.log('Sorry, paper covers rock.');
+	} else if (playerChoice === 'paper' && computerChoice === 'rock') {
+		console.log('Yay! Paper covers rock!');
+	} else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+		console.log('Sorry, rock beats scissors.');
+	} else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+		console.log('Yay! Rock beats scissors!');
+	} else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+		console.log('Yay! Scissors cut paper!');
+	} else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+		console.log('Sorry, scissors cut paper.');
+	} else if (playerChoice === computerChoice) {
+		console.log("It's a tie!");
+	}
+	return {
+		playerChoice,
+		computerChoice
+	};
 }
 
-// function game() {
-//     let fiveRounds = playRound();
-
-//     for (let i = 0; i < 5; i++) {
-//         game(i);
-//     }
-// }
-
-// Problem:
-// when game() is called
-//   store result playRound() within fiveRounds
-//   start a loop with a length of 6 times
-//     call game
-//       store result playRound() within fiveRounds
-//       start a loop with a length of 6 times
-//         call game
-// ...etc, this will continue
+function game() {
+	for (let round = 0; round < 5; round++) {
+		console.log(playRound());
+	}
+}
