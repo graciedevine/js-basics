60c0a6495b9fe4c97396ea794adaf9890fd400f7
graciedevine <gracie.devine@gmail.com>
2021-06-08 16:15:32 -0400

    Added HTML buttons, but need to show player and computer choices & game results.

--- rps-game/scripts/main.js
+++ rps-game/scripts/main.js
@@ -1,23 +1,9 @@
-// document.getElementById('rock').onclick = user;
-// document.getElementById('paper').onclick = user;
-// document.getElementById('scissors').onclick = user;
-
-// function user() {
-// 	let userChoice = this.id;
-// 	console.log(this.id);
-// 	return playRound(userChoice, getComputerMove())
-
-// }
-
 const buttons = document.querySelectorAll('button');
 
-// we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
-
-  // and for each one we add a 'click' listener
-  button.addEventListener('click', () => {
-    playRound(button.id, getComputerMove());
-  });
+	button.addEventListener('click', () => {
+		playRound(button.id, getComputerMove());
+	});
 });
 
 function getComputerMove() {
@@ -27,15 +13,17 @@ function getComputerMove() {
 }
 
 function playRound(playerChoice, computerChoice) {
-	console.log('This is player choice: ' + playerChoice)
-	console.log('This is computer choice: ' + computerChoice)
+	console.log('Player chose: ' + playerChoice)
+	console.log('Computer chose: ' + computerChoice)
 
 	if (playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors') {
 		alert('Invalid move.');
 		return;
 	}
 
-	if (playerChoice === 'rock' && computerChoice === 'paper') {
+	if (playerChoice === computerChoice) {
+		console.log("It's a tie!");
+	} else if (playerChoice === 'rock' && computerChoice === 'paper') {
 		console.log('Sorry, paper covers rock.');
 	} else if (playerChoice === 'paper' && computerChoice === 'rock') {
 		console.log('Yay! Paper covers rock!');
@@ -44,11 +32,9 @@ function playRound(playerChoice, computerChoice) {
 	} else if (playerChoice === 'rock' && computerChoice === 'scissors') {
 		console.log('Yay! Rock beats scissors!');
 	} else if (playerChoice === 'scissors' && computerChoice === 'paper') {
-		console.log('Yay! Scissors cut paper!');
+		console.log('Yay! Scissors cuts paper!');
 	} else if (playerChoice === 'paper' && computerChoice === 'scissors') {
-		console.log('Sorry, scissors cut paper.');
-	} else if (playerChoice === computerChoice) {
-		console.log("It's a tie!");
+		console.log('Sorry, scissors cuts paper.');
 	}
 	return {
 		playerChoice,
@@ -56,8 +42,8 @@ function playRound(playerChoice, computerChoice) {
 	};
 }
 
-function game() {
-	for (let round = 0; round < 5; round++) {
-		playRound();
-	}
-}
+// function game() {
+// 	for (let round = 0; round < 5; round++) {
+// 		playRound();
+// 	}
+// }
